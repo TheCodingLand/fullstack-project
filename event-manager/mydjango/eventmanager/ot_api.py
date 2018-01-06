683c6003fcc7a1ba77984ad848525993c1401d0f
1337Wolfpack <aureussz@gmail.com>
2018-01-06 17:57:47 +0100

    ok

--- event-manager/mydjango/eventmanager/ot_api.py
+++ event-manager/mydjango/eventmanager/ot_api.py
@@ -1,90 +1,91 @@
 import os
 import requests
-''' class Category(models.Model):
-    title = models.CharField(max_length=200, null=True)
-    predecessor = models.IntegerField(null=True, blank=True)
-    searchcode = models.CharField(max_length=200, null=True)
-    ot_id = models.CharField(max_length=200, null=True)
-
-    
-
-creationdate = models.DateTimeField(null=True)
-    end = models.DateTimeField(null=True, blank=True)
-    ot_id = models.IntegerField(null=True, blank=True)
-    applicant = models.ForeignKey(
-        Agent, related_name='events_applicant', on_delete=models.CASCADE, null=True, blank=True)
-    responsible = models.ForeignKey(
-        Agent, related_name='events_responsible', on_delete=models.CASCADE, null=True, blank=True)
-    state = models.CharField(max_length=200, null=True, blank=True)
-    transferhistory = models.CharField(max_length=200, null=True, blank=True)
-    phone = models.CharField(max_length=200, null=True, blank=True)
-    ticket = models.ForeignKey(
-        Ticket, related_name='tickets', on_delete=models.CASCADE, null=True, blank=True)
-    call = models.ForeignKey(
-        Call, on_delete=models.SET_NULL, null=True, related_name='event', blank=True)
-
- '''
 
 
-class ot_api(object):
+class ot_api_event(object):
     def __init__(self):
         self.url = 'http://ot-ws:5000/api/ot/'
 
-    def get_event_by_ot_id(self, objectName, id):
-        self.url = "%s/%s/id/%s" % (self.url, objectName, id)
-        resp = requests.get(url=url)
-        ot = json.loads(resp.text)
-        log.info(resp.text)
-        if resp.status_code == 404:
-            return False
+    def get_ot_id_from_call(id):
+        event = Events.object.get(call=call)
+        return event.ot_id
 
-        elif resp.status_code == 200:
-            ot = json.loads(resp.text)
-            log.info(ot.get('id'))
-            if ot['id'] != 0:
-                event = Event.objects.get_or_create(ot_id=ot['id'])[0]
-                event.save()
-            return event
-        return False
+    def create_event(self, callobj):
 
-    def create(self, objectName, callObject):
-
-        payload = {"UCID": "12312323",
-                   "Applicant": "Centrale", "Responsible": "Centale"}
-
-        self.url = "%s/events" % self.url
-        resp = requests.post(url, payload)
+        payload = {"UCID": call.ucid,
+                   "Applicant": "Centrale", "Responsible": "Centale", "Source": "Call Incoming"}
+        url = "%s/events" % self.url
+        req = requests.post(url, payload)
         try:
-            resp.get('id')
+            id = req.get('id')
         except KeyError:
             log.error("Could not create Event with payload %s" % payload)
+            return False
+        url = "%s/%s" % (url, id)
+        payload = {"CreationDate": call.start}
+        req = requests.post(url, payload)
 
-        {
-            "UCID": "string",
-            "CreationDate": "2018-01-06T15:37:11.961Z",
-            "Call Finished Date": "2018-01-06T15:37:11.961Z",
-            "Applicant": "login name from an agent",
-            "Responsible": "login name from an agent",
-            "State": "a state",
-            "RelatedIncident": 0
-        }
-        self.url = "%s/%s/event/%s" % (self.url, objectName, id)
-        resp = requests.get(url=url)
+        event = Events.objects.get_or_create(ot_id=id)
+        event.call = callobj
+        event.save()
 
-        event = Event.objects.get_or_create(ot_id=ot['id'])[0]
-        self.get(event, call)
-        event = Event.objets.get(call=callObject)
+        # good to have the same date for the event as the start of the call
 
-        self.url = "%s/%s/id/%s" % (self.url, objectName, id)
-        resp = requests.get(url=url)
+    def updateApplicant(self, call, agent):
+        payload = {"Applicant": agent.ot_userdisplayname}
+        url = "%s/events/%s" % (self.url, id)
 
-        origin = models.CharField(max_length=200, null=True)
-        destination = models.CharField(max_length=200, null=True)
-        call_type = models.CharField(max_length=200, null=True)
-        start = models.DateTimeField(max_length=200, null=True)
-        end = models.DateTimeField(max_length=200, null=True, blank=True)
-        isContactCenterCall = models.BooleanField(default=False)
-        history = models.CharField(max_length=600, null=True)
-        call.
-        self.get(objectName, )
-        'http://ot-ws:5000/api/ot/events/events/ucid/%s' % call.ucid
+        if req.status_code == 404:
+            return False
+        elif req.status_code == 200:
+            event = Events.object.get()
+            log.info("updated applicant to %s" % agent.ot_userdisplayname)
+            return True
+
+    def updateResponsible(self, call, agent):
+        payload = {"Responsible": agent.ot_userdisplayname}
+        url = "%s/events/%s" % (self.url, objecttype, id)
+        if req.status_code == 404:
+            return False
+        elif req.status_code == 200:
+            log.info("updated responsible to %s" % agent.ot_userdisplayname)
+            return True
+
+    def updateEndDate(self, call):
+        payload = {"Call Finished Date": call.end}
+        url = "%s/events/%s" % (self.url, id)
+
+        if req.status_code == 404:
+            return False
+        elif req.status_code == 200:
+            log.info("updated end date to %s" % agent.ot_userdisplayname)
+            return True
+
+    def updateEventPhoneNumber(self, call):
+        payload = {"Phone Number": call.origin}
+        url = "%s/events/%s" % (self.url, objecttype, id)
+
+        if req.status_code == 404:
+            return False
+        elif req.status_code == 200:
+            log.info("updated event phone number to %s" % call.origin)
+            return True
+
+    def updateEventHistory(self, call):
+        payload = {"TransferHistory": call.history}
+        url = "%s/events/%s" % (self.url, id)
+
+        if req.status_code == 404:
+            return False
+        elif req.status_code == 200:
+            log.info("updated event history to %s" % call.history)
+            return True
+
+    def updateEventType(self, call):
+        payload = {"Title": call.call_type}
+        url = "%s/events/%s" % (self.url, id)
+        if req.status_code == 404:
+            return False
+        elif req.status_code == 200:
+            log.info("updated event history to %s" % call.call_type)
+            return True
