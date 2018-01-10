ed23cffc5df5d7b795d3e5c2351785718567d9b9
Julien Le Bourg <aureussz@gmail.com>
2018-01-10 09:29:50 +0100

    added graphql queries for events and tickets

--- event-manager/mydjango/graphqlendpoint/schema.py
+++ event-manager/mydjango/graphqlendpoint/schema.py
@@ -3,13 +3,13 @@ from graphene import relay, ObjectType, AbstractType
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .models import Call, Agent
+from .models import Call, Agent, Event, Ticket
 
 
-class CallNode(DjangoObjectType):
+class TicketNode(DjangoObjectType):
     class Meta:
-        model = Call
-        filter_fields = ['ucid', 'origin','destination','state']
+        model = Ticket
+        filter_fields = ['title', 'state','solution','responsible','ot_id']
         interfaces = (relay.Node,)
         
         
@@ -20,9 +20,25 @@ class AgentNode(DjangoObjectType):
         interfaces = (relay.Node,)
 
 
+class CallNode(DjangoObjectType):
+    class Meta:
+        model = Call
+        filter_fields = ['ucid', 'origin','destination','state']
+        interfaces = (relay.Node,)
+
+
+class EventNode(DjangoObjectType):
+    class Meta:
+        model = Event
+        filter_fields = ['ot_id', 'applicant']
+        interfaces = (relay.Node,)
+
+
 class QueryCalls(object):
     calls = relay.Node.Field(CallNode)
     all_calls = DjangoFilterConnectionField(CallNode)
+
+
 
 
 class QueryAgents(object):
@@ -30,8 +46,16 @@ class QueryAgents(object):
     all_agents = DjangoFilterConnectionField(AgentNode)
         
 
-   
+
+class QueryEvents(object):
+    agents = graphene.List(EventNode)
+    all_events = DjangoFilterConnectionField(EventNode)
 
     
+
+
+class QueryTickets(object):
+    agents = graphene.List(TicketNode)
+    all_agents = DjangoFilterConnectionField(TicketNode)
 
 
